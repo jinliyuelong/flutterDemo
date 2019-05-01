562a4f8e47211dd970bf2add3e1d16b609eedcce
liyanjun <yanjun.li@hand-china.com>
2019-05-01 19:32:29 +0800

    增加安卓沉浸导航

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
+
 import 'layout/basicsLayout.dart';
+
 import 'navigation/navigationPractice.dart';
 import 'utils/utility.dart';
-void main() => runApp(MaterialApp(
+void main() {
+  runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'flutter学习demo',
       theme: ThemeData.light(),
       home: MyHomeScreen(),
     ));
+    // 以下两行 设置android状态栏为透明的沉浸。写在组件渲染之后，是为了在渲染后进行set赋值，覆盖状态栏，写在渲染之前MaterialApp组件会覆盖掉这个值。
+  if (Platform.isAndroid) {
+    SystemUiOverlayStyle systemUiOverlayStyle = SystemUiOverlayStyle(statusBarColor: Colors.transparent);
+    SystemChrome.setSystemUIOverlayStyle(systemUiOverlayStyle);
+  }
+}
 
 
 class MyHomeScreen extends StatelessWidget {
